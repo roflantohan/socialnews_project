275f46ba6507ecd778187107cc8a67b9e9a62b9e
roflantohan <anton.bezz.v@gmail.com>
2021-01-21 04:29:31 +0200

    fix: set eslint parser babel

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const fastify = require('fastify')({
 const path = require('path');
 
 const port = process.env.port || 3000;
-const TIMER = 0.5 * 60 * 1000;
+//const TIMER = 0.5 * 60 * 1000;
 
 fastify.register(require('./routes/api'));
 fastify.register(require('fastify-static'), {
